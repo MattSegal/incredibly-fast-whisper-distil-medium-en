0833727bcbf1eedfccff174886e82a145e8fc4bd
Matthew Segal <matt.segal@biarri.com>
2024-02-02 19:09:18 +1100

    use large v2

--- predict.py
+++ predict.py
@@ -10,8 +10,8 @@ class Predictor(BasePredictor):
         """Loads whisper models into memory to make running multiple predictions efficient"""
         self.model_cache = "model_cache"
         # model_id="distil-whisper/distil-small.en"
-        model_id="distil-whisper/distil-medium.en"
-        # model_id="distil-whisper/distil-large-v2"
+        # model_id="distil-whisper/distil-medium.en"
+        model_id="distil-whisper/distil-large-v2"
         # model_id="openai/whisper-large-v3"
         torch_dtype = torch.float16
         self.device = "cuda:0"
